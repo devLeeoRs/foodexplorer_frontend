8b803c9be6d4377ec37d250e832bec2d0ad59c91
Leonardo <devleeors@gmail.com>
2023-11-04 23:26:10 -0300

    adjust vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: ["react-icons/FI"],
+    },
+  },
   plugins: [react()],
   server: {
     host: "0.0.0.0",

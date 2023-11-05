f03fa66164d27ec1d1efe5529ab0f19dc7500098
Leonardo <devleeors@gmail.com>
2023-11-04 23:29:21 -0300

    adjust error icon fi

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: ["react-icons/FI"],
-    },
-  },
   plugins: [react()],
   server: {
     host: "0.0.0.0",
